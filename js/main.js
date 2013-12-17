896d6c4fc6d446aedd80f702133bc896f7f0ae34
xiaoF <870501@qq.com>
2013-12-17 18:44:41 +0800

    only blog modules commited

---
+++ js/main.js
@@ -0,0 +1,285 @@
+//百度统计
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?8701375826723deeac8ae97575bb13f2";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
+//多说
+var duoshuoQuery = {short_name:"fatcoders"};
+	(function() {
+		var ds = document.createElement('script');
+		ds.type = 'text/javascript';ds.async = true;
+		ds.src = 'http://static.duoshuo.com/embed.js';
+		ds.charset = 'UTF-8';
+		(document.getElementsByTagName('head')[0] 
+		|| document.getElementsByTagName('body')[0]).appendChild(ds);
+	})();
+
+jQuery(function($){
+
+var FATCODERS = window.FATCODERS || {};
+
+/* ==================================================
+   Super Navigation
+================================================== */
+FATCODERS.superNav = function(){
+    var button = document.getElementById('cn-button'),
+        wrapper = document.getElementById('cn-wrapper'),
+        overlay = document.getElementById('cn-overlay');
+
+    //open and close menu when the button is clicked
+    var open = false;
+    button.addEventListener('click', handler, false);
+    wrapper.addEventListener('click', cnhandle, false);
+
+    function cnhandle(e){
+        e.stopPropagation();
+    }
+
+    function handler(e){
+        if (!e) var e = window.event;
+        e.stopPropagation();//so that it doesn't trigger click event on document
+
+        if(!open){
+            openNav();
+        }
+        else{
+            closeNav();
+        }
+    }
+    function openNav(){
+        open = true;
+        button.innerHTML = "-";
+        classie.add(overlay, 'on-overlay');
+        classie.add(wrapper, 'opened-nav');
+    }
+    function closeNav(){
+        open = false;
+        button.innerHTML = "+";
+        classie.remove(overlay, 'on-overlay');
+        classie.remove(wrapper, 'opened-nav');
+    }
+    document.addEventListener('click', closeNav);
+}
+
+/* ==================================================
+   Slider Options
+================================================== */
+
+FATCODERS.slider = function(){
+	$.supersized({
+		// Functionality
+		slideshow               :   1,			// Slideshow on/off
+		autoplay				:	1,			// Slideshow starts playing automatically
+		start_slide             :   1,			// Start slide (0 is random)
+		stop_loop				:	0,			// Pauses slideshow on last slide
+		random					: 	0,			// Randomize slide order (Ignores start slide)
+		slide_interval          :   12000,		// Length between transitions
+		transition              :   1, 			// 0-None, 1-Fade, 2-Slide Top, 3-Slide Right, 4-Slide Bottom, 5-Slide Left, 6-Carousel Right, 7-Carousel Left
+		transition_speed		:	300,		// Speed of transition
+		new_window				:	1,			// Image links open in new window/tab
+		pause_hover             :   0,			// Pause slideshow on hover
+		keyboard_nav            :   1,			// Keyboard navigation on/off
+		performance				:	1,			// 0-Normal, 1-Hybrid speed/quality, 2-Optimizes image quality, 3-Optimizes transition speed // (Only works for Firefox/IE, not Webkit)
+		image_protect			:	1,			// Disables image dragging and right click with Javascript
+												   
+		// Size & Position						   
+		min_width		        :   0,			// Min width allowed (in pixels)
+		min_height		        :   0,			// Min height allowed (in pixels)
+		vertical_center         :   1,			// Vertically center background
+		horizontal_center       :   1,			// Horizontally center background
+		fit_always				:	0,			// Image will never exceed browser width or height (Ignores min. dimensions)
+		fit_portrait         	:   1,			// Portrait images will not exceed browser height
+		fit_landscape			:   0,			// Landscape images will not exceed browser width
+												   
+		// Components							
+		slide_links				:	'blank',	// Individual links for each slide (Options: false, 'num', 'name', 'blank')
+		thumb_links				:	0,			// Individual thumb links for each slide
+		thumbnail_navigation    :   0,			// Thumbnail navigation
+		slides 					:  	[			// Slideshow Images
+											{image : '../img/slider-images/image01.jpg', title : '<div class="slide-content">FATCODERS</div>', thumb : '', url : ''},
+											{image : '../img/slider-images/image02.jpg', title : '<div class="slide-content">FATCODERS</div>', thumb : '', url : ''},
+											{image : '../img/slider-images/image03.jpg', title : '<div class="slide-content">FATCODERS</div>', thumb : '', url : ''},
+											{image : '../img/slider-images/image04.jpg', title : '<div class="slide-content">FATCODERS</div>', thumb : '', url : ''}  
+									],
+									
+		// Theme Options			   
+		progress_bar			:	0,			// Timer for each slide							
+		mouse_scrub				:	0
+		
+	});
+
+}
+
+
+/* ==================================================
+   Next Section
+================================================== */
+
+FATCODERS.goSection = function(){
+	$('#nextsection').on('click', function(){
+		$target = $($(this).attr('href')).offset().top-30;
+		
+		$('body, html').animate({scrollTop : $target}, 750, 'easeOutExpo');
+		return false;
+	});
+}
+
+/* ==================================================
+   GoUp
+================================================== */
+
+FATCODERS.goUp = function(){
+	$('#goUp').on('click', function(){
+		$target = $($(this).attr('href')).offset().top-30;
+		
+		$('body, html').animate({scrollTop : $target}, 750, 'easeOutExpo');
+		return false;
+	});
+}
+
+
+/* ==================================================
+	Scroll to Top
+================================================== */
+
+FATCODERS.scrollToTop = function(){
+	var windowWidth = $(window).width(),
+		didScroll = false;
+
+	var $arrow = $('#back-to-top');
+
+	$arrow.click(function(e) {
+		$('body,html').animate({ scrollTop: "0" }, 750, 'easeOutExpo' );
+		e.preventDefault();
+	})
+
+	$(window).scroll(function() {
+		didScroll = true;
+	});
+
+	setInterval(function() {
+		if( didScroll ) {
+			didScroll = false;
+
+			if( $(window).scrollTop() > 1000 ) {
+				$arrow.css('display', 'block');
+			} else {
+				$arrow.css('display', 'none');
+			}
+		}
+	}, 250);
+}
+
+/* ==================================================
+   Thumbs / Social Effects
+================================================== */
+
+FATCODERS.utils = function(){
+	
+	$('.item-thumbs').bind('touchstart', function(){
+		$(".active").removeClass("active");
+      	$(this).addClass('active');
+    });
+	
+	$('.image-wrap').bind('touchstart', function(){
+		$(".active").removeClass("active");
+      	$(this).addClass('active');
+    });
+	
+	$('#social ul li').bind('touchstart', function(){
+		$(".active").removeClass("active");
+      	$(this).addClass('active');
+    });
+	
+}
+
+/* ==================================================
+   Accordion
+================================================== */
+
+FATCODERS.accordion = function(){
+	var accordion_trigger = $('.accordion-heading.accordionize');
+	
+	accordion_trigger.delegate('.accordion-toggle','click', function(event){
+		if($(this).hasClass('active')){
+			$(this).removeClass('active');
+		   	$(this).addClass('inactive');
+		}
+		else{
+		  	accordion_trigger.find('.active').addClass('inactive');          
+		  	accordion_trigger.find('.active').removeClass('active');   
+		  	$(this).removeClass('inactive');
+		  	$(this).addClass('active');
+	 	}
+		event.preventDefault();
+	});
+}
+
+/* ==================================================
+   Toggle
+================================================== */
+
+FATCODERS.toggle = function(){
+	var accordion_trigger_toggle = $('.accordion-heading.togglize');
+	
+	accordion_trigger_toggle.delegate('.accordion-toggle','click', function(event){
+		if($(this).hasClass('active')){
+			$(this).removeClass('active');
+		   	$(this).addClass('inactive');
+		}
+		else{
+		  	$(this).removeClass('inactive');
+		  	$(this).addClass('active');
+	 	}
+		event.preventDefault();
+	});
+}
+
+/* ==================================================
+   Tooltip
+================================================== */
+
+FATCODERS.toolTip = function(){ 
+    $('a[data-toggle=tooltip]').tooltip();
+}
+
+
+/* ==================================================
+	Init
+================================================== */
+
+FATCODERS.slider();
+
+$(document).ready(function(){
+
+	$('body').jpreLoader({
+		splashID: "#jSplash",
+		showSplash: true,
+		showPercentage: true,
+		autoClose: true,
+		splashFunction: function() {
+			$('#circle').delay(250).animate({'opacity' : 1}, 500, 'linear');
+		}
+	});
+
+	// FATCODERS.superNav();
+	FATCODERS.goSection();
+	FATCODERS.goUp();
+	FATCODERS.scrollToTop();
+	FATCODERS.utils();
+	FATCODERS.accordion();
+	FATCODERS.toggle();
+	FATCODERS.toolTip();
+});
+
+});
+	$('#logo').click(function(event) {
+		
+		$('#logo').animo("rotate", { degrees: -90,duration: 2},function(){
+			$('#logo').animo("rotate", { degrees: 0,duration: 2})
+		});
+	});
